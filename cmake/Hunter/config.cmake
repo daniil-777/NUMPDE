1f4cd9ed17888bc0a0b38f0340636b87ef322277
Daniil <daniil.emcev.ru@yandex.ru>
2021-01-14 20:20:49 +0300

    cmake files for installation

---
+++ cmake/Hunter/config.cmake
@@ -0,0 +1,11 @@
+# This file specifies additional data for the dependencies that are imported via hunter
+hunter_config(Boost VERSION 1.66.0)
+hunter_config(Eigen VERSION 3.3.8-b11f817)
+
+# hunter_config(lehrfempp VERSION 0.7.21)
+hunter_config(lehrfempp
+  URL "https://github.com/craffael/lehrfempp/archive/bf7c8bc2c8c06910a833d2ac010d9d48be3fdaed.tar.gz"
+  SHA1 "ee5527d403ff4cc03caa83c691caaaaf033bf75d"
+)
+
+hunter_config(GTest VERSION 1.10.0)
